34f92f5732cfe4385a7e57328a51f9176f6d1dad
Senir Sales Oliveira <senirsales@hotmail.com>
2022-05-21 12:52:50 -0300

    CRUD

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,5 +1,6 @@
 package me.dio.academia.digital.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_avaliacoes")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class AvaliacaoFisica {
 
   @Id
